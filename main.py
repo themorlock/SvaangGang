c99d89042ceeb1df204608d062d210b85a353932
themorlock <saaketh.vangati@gmail.com>
2018-02-10 17:02:32 -0800

    Add files via upload

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import bitmex
 import datetime
 import time
 import _thread
- 
-#API_KEY = 'Bxv6_Fe4q32NBpPRZB7e5hRt'
-#API_SECRET = 'mmyKA0cITGghroSFMUtM46pULsuRdxvgp_LKF0jCscLnbrsu'
-API_KEY = 'YV109Til3xUBwcDX36sTwnLH'
-API_SECRET = 'w-nY31B7wOJ8SRlJCJ4Rfc6rj0nUnlWfJ1J9g59NPmjsL1KQ'
+
+API_KEY = 'Bxv6_Fe4q32NBpPRZB7e5hRt'
+API_SECRET = 'mmyKA0cITGghroSFMUtM46pULsuRdxvgp_LKF0jCscLnbrsu'
+#API_KEY = 'YV109Til3xUBwcDX36sTwnLH'
+#API_SECRET = 'w-nY31B7wOJ8SRlJCJ4Rfc6rj0nUnlWfJ1J9g59NPmjsL1KQ'
 SYMBOL = 'XBTUSD'
-IS_TEST = False
+IS_TEST = True
 RSI_PERIOD_MINUTE = (1 / 6)
 RSI_CALCULATION_PERIOD = 14
 client = 0
 rsi = 0
- 
- 
+balance = .1
+
+
 def rsi_thread():
     global client
     global rsi
@@ -38,36 +39,46 @@ def rsi_thread():
                     downs.append(abs(difference))
                 else:
                     downs.append(0)
- 
+
                 ups_average = sum(ups) / len(ups)
                 downs_average = sum(downs) / len(downs)
- 
+
                 if downs_average == 0:
-                    downs_average = 1;
+                    downs_average = 1
                 rs = ups_average / downs_average
                 rsi = 100 - (100 / (1 + rs))
- 
+
         time.sleep(RSI_PERIOD_MINUTE * 60)
- 
- 
+
+
 def get_rsi():
     return rsi
- 
- 
+
+def get_current_price():
+    global client
+    return float(client.Quote.Quote_get(symbol=SYMBOL, reverse=True, count=1).result()[0][0]['bidPrice'])
+
 def main():
     global client
     print('{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()), 'Starting System...')
     client = bitmex.bitmex(test=IS_TEST, api_key=API_KEY, api_secret=API_SECRET)
     print('{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()), 'Connected To Servers!')
- 
+
     print('{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()), 'System will start in', str(RSI_PERIOD_MINUTE * RSI_CALCULATION_PERIOD), 'minutes.')
     _thread.start_new_thread(rsi_thread, ())
     time.sleep((RSI_PERIOD_MINUTE * RSI_CALCULATION_PERIOD * 60) + RSI_CALCULATION_PERIOD)
     while True:
-        print('{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()), 'Current RSI is', str(get_rsi()))
+        current_rsi = get_rsi()
+        print('{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()), 'Current RSI is', str(current_rsi))
+        if current_rsi >= 70:
+            print('{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()), 'Selling...')
+            client.Order.Order_new(symbol=SYMBOL, orderQty=60, price=get_current_price()).result()
+        elif current_rsi <= 30:
+            print('{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()), 'Buying...')
+            client.Order.Order_new(symbol=SYMBOL, orderQty=60, price=get_current_price()).result()
+        else:
+            print('{:%Y-%m-%d %H:%M:%S}'.format(datetime.datetime.now()), 'Holding')
         time.sleep(RSI_PERIOD_MINUTE * 60)
- 
 
-if __name__ == "__main__":
-    main()
-    
+
+main()
