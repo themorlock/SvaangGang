ac08621d56bc568efac1158840ad7e82bc6caa80
Lokraan <Artinaire@gmail.com>
2018-02-20 00:25:21 -0600

    Improved Simulator and trading algo.
    Trading simulator prints out the resulting data. From this we found that
    an rsi of 4 is the best. In light of this we are now using an rsi of 4 for
    our trading algo. Trading algo is currently modified for bitmex, but will
    receive compatability for other exchanges.

--- simulator/simulator_bot.py
+++ simulator/simulator_bot.py
@@ -2,29 +2,69 @@
 from datetime import datetime
 import math
 import json
+import csv
 import sys
 
 sys.path.append("../helpers")
 
-import processor
-
 
 DATA_FILE = "data.json"
-RSI_PERIOD = 7
-
 
 def load_data(file):
 	with open(file, "r") as f:
 		return json.load(f)
 
 
-def lin_buy_func(base_size: int, hits: int, k: int = 4):
+def lin_buy_func(base_size: int, hits: int, k: int = 1.61):
 	return base_size * (k*hits + 1)
 
 
 def sig_buy_func(max_balance: float, hits: int, k: int = 1):
 	return max_balance / (1 + math.exp(-(k*hits - 5)))
 						  
+
+def calc_rsi(data: list, period: int = 14) -> int:
+
+	max_len = period if period < len(data) else len(data)
+
+	prices = [i[4] for i in data]
+
+	losses = []
+	gains = []
+
+	for i in range(1, max_len):
+		try:
+			change = prices[i] - prices[i-1]
+			if change < 0:
+				losses.append(abs(change))
+			elif change > 0:
+				gains.append(change)
+		except TypeError as e:
+			print(e + " " + prices)
+
+
+	avg_loss = sum(losses) / period
+	avg_gain = sum(gains) / period
+
+	for i in range(period, len(prices)):
+		change = prices[i] - prices[i - 1]
+		loss = abs(change) if change < 0 else 0
+		gain = change if change > 0 else 0
+
+		avg_gain = (avg_gain * (period - 1) + gain) / period
+		avg_loss = (avg_loss * (period - 1) + gain) / period
+
+	if avg_loss == 0:
+		return 100
+
+	elif avg_gain == 0:
+		return 0
+
+	rs = avg_gain / avg_loss
+	rsi = round(100 - (100 / (1 + rs)), 2)
+
+	return rsi
+
 
 def run_simul(data: list, buy_func, btc_bal: float = 0.1, usd_bal: float = 100,
 		order_size: float = 0.1, rsi_sell: int = 70,
@@ -35,10 +75,10 @@ def run_simul(data: list, buy_func, btc_bal: float = 0.1, usd_bal: float = 100,
 	buy_hits = 0
 
 	if return_hist:
-		hist = {}
+		hist = []
 
 	for i in range(orders_per_rsi, len(data)):
-		rsi = processor.calc_rsi(data[i - orders_per_rsi:i], rsi_period)
+		rsi = calc_rsi(data[i - orders_per_rsi:i], rsi_period)
 
 		price = data[i][4]
 
@@ -71,7 +111,14 @@ def run_simul(data: list, buy_func, btc_bal: float = 0.1, usd_bal: float = 100,
 				#  print("selling {0} hits {1}".format(amt*price, sell_hits-1))
 
 		if return_hist:
-			hist[data[i][0]] = {"usd_bal": usd_bal, "btc_bal": btc_bal}
+			hist.append({
+				"timestamp": data[i][0]/1000,
+				"usd_bal": usd_bal, 
+				"btc_bal": btc_bal,
+				"total_btc_bal": btc_bal + (usd_bal / price),
+				"total_usd_bal": usd_bal + (btc_bal * price),
+				"btc_price": price 
+				})
 
 	if return_hist:
 		return (btc_bal, usd_bal, hist)
@@ -80,37 +127,47 @@ def run_simul(data: list, buy_func, btc_bal: float = 0.1, usd_bal: float = 100,
 			  
 
 def main():
-	btc_bal = 0.1
-	usd_bal = 100
+	init_btc_bal = 0.1
+	init_usd_bal = 100
 
 	size = 0.1 # percent
 
 	sell = 70
 	buy = 30
 
-	order_size = [0.05*i for i in range(1, 10)]
-	rsi_period = [i for i in range(15)]
+	# stuff to test
+	rsi_periods = [i for i in range(2, 15)]
+	order_sizes = [0.12]
 
 	data = load_data(DATA_FILE)[::-1]
 
 	price = float(data[0][4])
 
-	# print("Start!\nBtc Bal: {0: < 6} | Usd Bal: {1: < 6} | Total Btc: {2: < 6} | Total Usd: {3: < 6}".format(
-	# 		round(btc_bal, 4), round(usd_bal, 4), 
-	# 		round(btc_bal + usd_bal / price, 4),
-	# 		round(btc_bal * price + usd_bal, 4)))
+	print("Start!\nBtc Bal: {0: < 10} | Usd Bal: {1: < 10} \
+		| Total Btc: {2: < 10} | Total Usd: {3: < 10}".format(
+		round(init_btc_bal, 4), round(init_usd_bal, 4), 
+		round(init_btc_bal + init_usd_bal / price, 4),
+		round(init_btc_bal * price + init_usd_bal, 4)))
 
-	for i, v in enumerate(rsi_period):
-		btc_bal, usd_bal = run_simul(data, lin_buy_func, btc_bal=btc_bal, usd_bal=usd_bal,
-			rsi_period=3, orders_per_rsi=3)
+	price = float(data[-1][4])
 
-	# price = data[-1][4]
-	# print("Endt\nBtc Bal: {0: < 6} | Usd Bal: {1: < 6} | Total Btc: {2: < 6} | Total Usd: {3: < 6}".format(
-	# 		round(btc_bal, 4), round(usd_bal, 4), 
-	# 		round(btc_bal + usd_bal / price, 4),
-	# 		round(btc_bal * price + usd_bal, 4)))
+	for period in rsi_periods:
+		for order_size in order_sizes:
 
-	# print(datetime.fromtimestamp(data[-1][0]/1000))
+			btc_bal, usd_bal, hist = run_simul(data, lin_buy_func, btc_bal=init_btc_bal,
+				usd_bal=init_usd_bal, rsi_period=period, orders_per_rsi=period, return_hist=True)
+
+			total_btc_bal = round(btc_bal + usd_bal / price, 6)
+			total_usd_bal = round(btc_bal * price + usd_bal, 2)
+			gain = round(total_btc_bal / init_btc_bal, 2)
+
+			print("\nRSI Period: {} Order Size: {} Finished".format(period, order_size))
+
+			text = "End\nBtc Bal: {0: < 6} | Usd Bal: {1: < 6} | Total Btc: {2: < 8} | Total Usd: {3: < 6} | Gain: {4: < 6}"
+			print(text.format(
+				round(btc_bal, 4), round(usd_bal, 4),
+				total_btc_bal, total_usd_bal, gain)
+			)
 
 
 if __name__ == '__main__':
--- trading_bot.py
+++ trading_bot.py
@@ -1,120 +1,123 @@
-from datetime import datetime, timedelta
+
+from datetime import datetime
 import asyncio
 import sys
 
 from ccxt import bitmex
 
+sys.path.append("helpers")
+import processor
+
+
 class Bot:
-    def __init__(self, config: dict, logger=None, client=None):
-        self._config = config
-        self._logger = logger
+	def __init__(self, config: dict, logger=None, client=None):
+		self._config = config
+		self._logger = logger
 
-        self._symbol = config["symbol"]
-        self._time_delay = config["time_delay"]
+		self._symbol = config["symbol"]
 
-        self._rsi_period_time = config["rsi_period_time"]
-        self._rsi_calculation_period = config["rsi_calculation_period"]
-        self._rsi_sell = config["rsi_sell"]
-        self._rsi_buy = config["rsi_buy"]
+		self._rsi_timeframe = config["rsi_timeframe"]
+		self._rsi_period = config["rsi_period"]
+		self._rsi_sell = config["rsi_sell"]
+		self._rsi_buy = config["rsi_buy"]
 
-        self._purchase_size_percentage = config["purchase_size_percentage"]
+		self._purchase_size_percentage = config["purchase_size_percentage"]
 
-        self._bot_refresh_speed = config["bot_refresh_speed"]
+		self._bot_refresh_speed = config["bot_refresh_speed"]
 
-        if client:
-            self._client = client
-        else:
-            self._client = bitmex({
-                "test": config["test"],
-                "apiKey": config["apiKey"],
-                "secret": config["secret"]
-            })
-
-
-    def _calculate_purchase_size(self, base_purchase_size: int, hits: int):
-        return base_purchase_size * (hits + 1)
-
-    
-    def _get_available_balance(self):
-        return self._client.fetch_balance()[self._symbol[:self._symbol.index("/")]]["free"]
+		if client:
+			self._client = client
+		else:
+			self._client = bitmex({
+				"test": config["test"],
+				"apiKey": config["apiKey"],
+				"secret": config["secret"]
+			})
 
 
-    def _get_current_price(self) -> float:
-        return self._client.fetch_ticker(self._symbol)["close"]
+	def _calculate_purchase_size(self, base_purchase_size: int, hits: int):
+		return base_purchase_size * (1.61*hits + 1)
+
+	
+	def _get_available_balance(self):
+		return self._client.fetch_balance()[self._symbol[:self._symbol.index("/")]]["free"]
 
 
-    def _get_historic_timestamp(self) -> int:
-        return (datetime.now() - timedelta(minutes=(self._rsi_period_time * (self._rsi_calculation_period + 1) + self._time_delay))).timestamp() * 1000
+	def _get_current_price(self) -> float:
+		return self._client.fetch_ticker(self._symbol)["close"]
 
 
-    def _get_historical_data(self):
-        prices = self._client.fetch_ohlcv(self._symbol, timeframe="1m", since=self._get_historic_timestamp())
-        close_prices = []
-        for price in prices:
-            close_prices.append(price[4])
-        return close_prices[::self._rsi_period_time]
+	def _get_historical_data(self):
+
+		dist = self._rsi_period * self._rsi_timeframe
+		base = self._client.fetch_ticker("BTC/USD")["timestamp"] / 1000
+		since = processor.get_timestamp(dist, base)
+
+		prices = self._client.fetch_ohlcv(self._symbol, "1m", since=since*1000)[:-1]
+
+		close_prices = [p[4] for p in prices]
+
+		self._logger.debug(close_prices)
+
+		return close_prices
 
 
-    def _calculate_rsi(self):
-        prices = self._get_historical_data()
+	async def start(self):
 
-        gains = []
-        losses = []
-        for i in range(1, len(prices)):
-            difference = prices[i] - prices[i - 1]
-            if difference < 0:
-                losses.append(abs(difference))
-            elif difference > 0:
-                gains.append(difference)
-        
-        avg_gain = sum(gains) / self._rsi_calculation_period
-        avg_loss = sum(losses) / self._rsi_calculation_period
+		sell_hits = 0
+		buy_hits = 0
 
-        if avg_loss == 0:
-            return 100
+		curr_bought = 0 
+		curr_sold = 0
 
-        rs = avg_gain / avg_loss
-        rsi = (100 - (100 / (1 + rs)))
-        return rsi
+		while True:
+			available_balance = self._get_available_balance()
+			current_price = self._get_current_price()
+
+			prices = self._get_historical_data()
+			current_rsi = processor.calculate_rsi(prices, self._rsi_period)
+
+			self._logger.info("RSI: is {}".format(current_rsi))
+
+			if current_rsi >= self._rsi_sell:
+				current_order_size = self._calculate_purchase_size(available_balance * self._purchase_size_percentage, sell_hits)
+
+				if current_order_size > available_balance:
+					current_order_size = available_balance
+
+				current_order_size = int(current_price * current_order_size)
+
+				self._logger.info("Selling {0: < 4} at {1}".format(current_order_size, self._get_current_price()))
+
+				self._client.create_market_sell_order(symbol=self._symbol, amount=current_order_size + curr_bought)
+
+				curr_sold += current_order_size
+				curr_bought = 0
+
+				sell_hits += 1
+				buy_hits = 0 
 
 
-    async def start(self):
+			elif current_rsi <= self._rsi_buy:
+				current_order_size = self._calculate_purchase_size(available_balance * self._purchase_size_percentage, buy_hits)
 
-        sell_hits = 0
-        buy_hits = 0
+				if current_order_size > available_balance:
+					current_order_size = available_balance
 
-        while True:
-            current_price = self._get_current_price()
-            available_balance = self._get_available_balance()
-            current_rsi = self._calculate_rsi()
+				current_order_size = int(current_price * current_order_size)
 
-            self._logger.info("RSI: is {}".format(current_rsi))
+				self._logger.info("Buying {0: < 4} at {1}".format(current_order_size, self._get_current_price()))
+				
+				self._client.create_market_buy_order(symbol=self._symbol, amount=current_order_size + curr_sold)
 
-            if current_rsi >= self._rsi_sell:
-                current_order_size = self._calculate_purchase_size(available_balance * self._purchase_size_percentage, sell_hits)
+				curr_bought += current_order_size
+				curr_sold = 0
 
-                if current_order_size < available_balance:
-                    current_order_size = int(current_order_size * current_price)
-                    self._logger.info("Selling {0: < 4} at {1}".format(current_order_size, self._get_current_price()))
-                    self._client.create_market_sell_order(symbol=self._symbol, amount=current_order_size)
-                else:
-                    self._logger.info("Holding because current balance has insufficient funds")
-                sell_hits += 1
-                buy_hits = 0
+				buy_hits += 1
+				sell_hits = 0
 
-            elif current_rsi <= self._rsi_buy:
-                current_order_size = self._calculate_purchase_size(available_balance * self._purchase_size_percentage, buy_hits)
+			
+			else:
+				self._logger.info("Holding")
 
-                if current_order_size < available_balance:
-                    current_order_size = int(current_order_size * current_price)
-                    self._logger.info("Buying {0: < 4} at {1}".format(current_order_size, self._get_current_price()))
-                    self._client.create_market_buy_order(symbol=self._symbol, amount=current_order_size)
-                else:
-                    self._logger.info("Holding because current balance has insufficient funds")
-                buy_hits += 1
-                sell_hits = 0
-            
-            else:
-                self._logger.info("Holding")
-
-            await asyncio.sleep(self._bot_refresh_speed)
+			await asyncio.sleep(self._bot_refresh_speed)
